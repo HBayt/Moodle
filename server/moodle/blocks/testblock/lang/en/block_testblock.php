ba740c4fc2fd7c93579a72ce80a5062d3747fb91
HBayt <battenberg.sf2024@gmail.com>
2024-11-06 10:55:45 +0100

    Instance Moodle - DEV 5
    Instance Moodle - vs Dev5

---
+++ server/moodle/blocks/testblock/lang/en/block_testblock.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Languages configuration for the block_pluginname plugin.
+ *
+ * @package   block_testblock
+ * @copyright 2024, H. Baytar <halide.baytar@battenberg.ch>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+//  @copyright Year, You Name <your@email.address>
+
+$string['pluginname'] = 'Block for student grade plugin';
+$string['pluginname'] = 'Student notes plugin';
+$string['pluginname:addinstance'] = 'Add a new pluginnote block';
+$string['pluginname:myaddinstance'] = 'Add a new PLUGINNOTES block to the My Moodle page'; 
+
+
+
+$string['configtitle'] = 'Text block title';
+$string['block_testblock:addinstance'] = 'Add a new test block';
+$string['block_testblock:myaddinstance'] = 'Add a new test block to Dashboard';
+$string['block_testblock'] = '(new test block)';
+$string['pluginname'] = 'Test block';
